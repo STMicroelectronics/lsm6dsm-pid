6fe729b78693cc616fa28037b5805d07371d42f3
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-01-13 12:48:25 +0100

    Change bit address representation for sensor-hub from 7 to 8
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- lsm6dsm_reg.c
+++ lsm6dsm_reg.c
@@ -8606,7 +8606,7 @@ int32_t lsm6dsm_sh_cfg_write(const stmdev_ctx_t *ctx,
 
   if (ret == 0)
   {
-    slv0_add.slave0_add = val->slv0_add;
+    slv0_add.slave0_add = (uint8_t)(val->slv0_add >> 1);
     slv0_add.rw_0 = 0;
     ret = lsm6dsm_write_reg(ctx, LSM6DSM_SLV0_ADD, (uint8_t *)&slv0_add, 1);
 
@@ -8650,7 +8650,7 @@ int32_t lsm6dsm_sh_slv0_cfg_read(const stmdev_ctx_t *ctx,
 
   if (ret == 0)
   {
-    slv0_add.slave0_add = val->slv_add;
+    slv0_add.slave0_add = (uint8_t)(val->slv_add >> 1);
     slv0_add.rw_0 = 1;
     ret = lsm6dsm_write_reg(ctx, LSM6DSM_SLV0_ADD, (uint8_t *)&slv0_add, 1);
 
@@ -8701,7 +8701,7 @@ int32_t lsm6dsm_sh_slv1_cfg_read(const stmdev_ctx_t *ctx,
 
   if (ret == 0)
   {
-    slv1_add.slave1_add  = val->slv_add;
+    slv1_add.slave1_add  = (uint8_t)(val->slv_add >> 1);
     slv1_add.r_1 = 1;
     ret = lsm6dsm_write_reg(ctx, LSM6DSM_SLV1_ADD, (uint8_t *)&slv1_add, 1);
 
@@ -8752,7 +8752,7 @@ int32_t lsm6dsm_sh_slv2_cfg_read(const stmdev_ctx_t *ctx,
 
   if (ret == 0)
   {
-    slv2_add.slave2_add  = val->slv_add;
+    slv2_add.slave2_add  = (uint8_t)(val->slv_add >> 1);
     slv2_add.r_2 = 1;
     ret = lsm6dsm_write_reg(ctx, LSM6DSM_SLV2_ADD, (uint8_t *)&slv2_add, 1);
 
@@ -8803,7 +8803,7 @@ int32_t lsm6dsm_sh_slv3_cfg_read(const stmdev_ctx_t *ctx,
 
   if (ret == 0)
   {
-    slv3_add.slave3_add  = val->slv_add;
+    slv3_add.slave3_add  = (uint8_t)(val->slv_add >> 1);
     slv3_add.r_3 = 1;
     ret = lsm6dsm_write_reg(ctx, LSM6DSM_SLV3_ADD, (uint8_t *)&slv3_add, 1);
 
